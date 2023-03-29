8cf11bc959252d9aa799accec5fc47ee6315ee57
unmeimusu <unmeimusu@gmail.com>
2023-03-30 04:43:30 +0700

    Update files

--- appm.js
+++ appm.js
@@ -1,4 +1,5 @@
-import Alpine from './cdn.min.js'
+// import Alpine from './node_modules/alpinejs/dist/module.esm.js0
+import Alpine from './module.esm.js'
 import dropdown from './d.js'
 
 Alpine.data('drpd', dropdown)
